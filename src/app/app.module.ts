fd37e4377e4b1a2f17bfef283415c90f1900085f
ozaki <fs65ugumori@yahoo.co.jp>
2020-02-11 02:27:06 +0900

    remove unused sns button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,42 +12,16 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { authReducer } from './auth/auth.reducer';
 import { environment } from '../environments/environment';
-import { LoginDirective } from './auth/login.directive';
 import { LogoutDirective } from './auth/logout.directive';
 
 const firebaseUiAuthConfig: firebaseui.auth.Config = {
   signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    {
-      scopes: ['public_profile', 'email', 'user_likes', 'user_friends'],
-      customParameters: {
-        auth_type: 'reauthenticate',
-      },
-      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    },
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    {
-      requireDisplayName: false,
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    },
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID,
-  ],
-  tosUrl: '<your-tos-link>',
-  privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
   credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
 };
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    LoginDirective,
-    LogoutDirective,
-  ],
+  declarations: [AppComponent, LoginComponent, HomeComponent, LogoutDirective],
   imports: [
     BrowserModule,
     AppRoutingModule,
